8ded1f8dddc804654d7574d38a68a3a98dc266ea
flylib <liushaomingdev@163.com>
2018-09-05 11:16:08 +0800

    二叉查找树- Binary Search Tree java implemented by Java

---
+++ src/main/java/com/appjishu/algorithm4j/binarysearchtree/BSTree.java
@@ -0,0 +1,193 @@
+package com.appjishu.algorithm4j.binarysearchtree;
+
+public class BSTree<T extends Comparable<T>> {
+
+    private BSTNode<T> mRoot;    // 根结点
+
+    public class BSTNode<T extends Comparable<T>> {
+        T key;                // 关键字(键值)
+        BSTNode<T> left;      // 左孩子
+        BSTNode<T> right;     // 右孩子
+        BSTNode<T> parent;    // 父结点
+
+        public BSTNode(T key, BSTNode<T> parent, BSTNode<T> left, BSTNode<T> right) {
+            this.key = key;
+            this.parent = parent;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /*
+     * 将结点插入到二叉树中
+     *
+     * 参数说明：
+     *     tree 二叉树的
+     *     z 插入的结点
+     */
+    private void insert(BSTree<T> bst, BSTNode<T> z) {
+        int cmp;
+        BSTNode<T> y = null;
+        BSTNode<T> x = bst.mRoot;
+
+        // 查找z的插入位置
+        while (x != null) {
+            y = x;
+            cmp = z.key.compareTo(x.key);
+            if (cmp < 0)
+                x = x.left;
+            else
+                x = x.right;
+        }
+
+        z.parent = y;
+        if (y == null)
+            bst.mRoot = z;
+        else {
+            cmp = z.key.compareTo(y.key);
+            if (cmp < 0)
+                y.left = z;
+            else
+                y.right = z;
+        }
+    }
+
+    /*
+     * 新建结点(key)，并将其插入到二叉树中
+     *
+     * 参数说明：
+     *     tree 二叉树的根结点
+     *     key 插入结点的键值
+     */
+    public void insert(T key) {
+        BSTNode<T> z = new BSTNode<T>(key, null, null, null);
+
+        // 如果新建结点失败，则返回。
+        if (z != null)
+            insert(this, z);
+    }
+
+    /*
+     * 找结点(x)的前驱结点。即，查找"二叉树中数据值小于该结点"的"最大结点"。
+     */
+    public BSTNode<T> predecessor(BSTNode<T> x) {
+        // 如果x存在左孩子，则"x的前驱结点"为 "以其左孩子为根的子树的最大结点"。
+        if (x.left != null)
+            return maximum(x.left);
+
+        // 如果x没有左孩子。则x有以下两种可能：
+        // (01) x是"一个右孩子"，则"x的前驱结点"为 "它的父结点"。
+        // (01) x是"一个左孩子"，则查找"x的最低的父结点，并且该父结点要具有右孩子"，找到的这个"最低的父结点"就是"x的前驱结点"。
+        BSTNode<T> y = x.parent;
+        while ((y != null) && (x == y.left)) {
+            x = y;
+            y = y.parent;
+        }
+
+        return y;
+    }
+
+    /*
+     * 找结点(x)的后继结点。即，查找"二叉树中数据值大于该结点"的"最小结点"。
+     */
+    public BSTNode<T> successor(BSTNode<T> x) {
+        // 如果x存在右孩子，则"x的后继结点"为 "以其右孩子为根的子树的最小结点"。
+        if (x.right != null)
+            return minimum(x.right);
+
+        // 如果x没有右孩子。则x有以下两种可能：
+        // (01) x是"一个左孩子"，则"x的后继结点"为 "它的父结点"。
+        // (02) x是"一个右孩子"，则查找"x的最低的父结点，并且该父结点要具有左孩子"，找到的这个"最低的父结点"就是"x的后继结点"。
+        BSTNode<T> y = x.parent;
+        while ((y != null) && (x == y.right)) {
+            x = y;
+            y = y.parent;
+        }
+
+        return y;
+    }
+
+    /*
+     * 查找最大结点：返回tree为根结点的二叉树的最大结点。
+     */
+    private BSTNode<T> maximum(BSTNode<T> tree) {
+        if (tree == null)
+            return null;
+
+        while (tree.right != null)
+            tree = tree.right;
+        return tree;
+    }
+
+    public T maximum() {
+        BSTNode<T> p = maximum(mRoot);
+        if (p != null)
+            return p.key;
+
+        return null;
+    }
+
+    /*
+     * 查找最小结点：返回tree为根结点的二叉树的最小结点。
+     */
+    private BSTNode<T> minimum(BSTNode<T> tree) {
+        if (tree == null)
+            return null;
+
+        while (tree.left != null)
+            tree = tree.left;
+        return tree;
+    }
+
+    public T minimum() {
+        BSTNode<T> p = minimum(mRoot);
+        if (p != null)
+            return p.key;
+
+        return null;
+    }
+
+    /*
+     * (递归实现)查找"二叉树x"中键值为key的节点
+     */
+    private BSTNode<T> search(BSTNode<T> x, T key) {
+        if (x == null)
+            return x;
+
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0)
+            return search(x.left, key);
+        else if (cmp > 0)
+            return search(x.right, key);
+        else
+            return x;
+    }
+
+    public BSTNode<T> search(T key) {
+        return search(mRoot, key);
+    }
+
+    /*
+     * (非递归实现)查找"二叉树x"中键值为key的节点
+     */
+    private BSTNode<T> iterativeSearch(BSTNode<T> x, T key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+
+            if (cmp < 0)
+                x = x.left;
+            else if (cmp > 0)
+                x = x.right;
+            else
+                return x;
+        }
+
+        return x;
+    }
+
+    public BSTNode<T> iterativeSearch(T key) {
+        return iterativeSearch(mRoot, key);
+    }
+
+
+}
