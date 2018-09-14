47d95720008daf1626c201d938692f7499e687bd
flylib <liushaomingdev@163.com>
2018-09-14 16:49:34 +0800

    寻找第K大的数

--- src/main/java/com/appjishu/algorithm4j/exam/FindKMax.java
+++ src/main/java/com/appjishu/algorithm4j/exam/FindKMax.java
@@ -16,53 +16,18 @@ public class FindKMax {
     }
 
     public static int findKMaxInteger(int[] a, int low, int high, int k) {
-//        int start = low;
-////        int end = high;
-////        int key = a[low];
-////
-////        while (end > start) {
-////            //从后往前比较
-////            while (end > start && a[end] >= key)  //如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
-////                end--;
-////            if (a[end] <= key) {
-////                int temp = a[end];
-////                a[end] = a[start];
-////                a[start] = temp;
-////            }
-////            //从前往后比较
-////            while (end > start && a[start] <= key)//如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
-////                start++;
-////            if (a[start] >= key) {
-////                int temp = a[start];
-////                a[start] = a[end];
-////                a[end] = temp;
-////            }
-////        }
-////        //此时第一次循环比较结束，关键值的位置已经确定了。左边的值都比关键值小，右边的值都比关键值大，但是两边的顺序还有可能是不一样的，进行下面的递归调用
-////
-////        if (a.length - start - 1 < k) {
-////            return findKMaxInteger(a, low, start - 1, k + start - a.length);
-////        } else if (a.length - start - 1 > k) {
-////            return findKMaxInteger(a, start + 1, high, k);
-////        } else {
-////            return start;
-////        }
-
         int n = a.length;
-        if(low >= high || k > n)//边界条件和特殊输入的处理
+        if (low >= high || k > n)//边界条件和特殊输入的处理
             return 0;
-        int pos = partition(a,low,high);
-        while(pos != n  - k)
-        {
-            if(pos > n - k)
-            {
+        int pos = partition(a, low, high);
+        while (pos != n - k) {
+            if (pos > n - k) {
                 high = pos - 1;
-                pos = partition(a,low,high);
+                pos = partition(a, low, high);
             }
-            if(pos < n - k)
-            {
+            if (pos < n - k) {
                 low = pos + 1;
-                pos = partition(a,low,high);
+                pos = partition(a, low, high);
             }
         }
         return pos;
